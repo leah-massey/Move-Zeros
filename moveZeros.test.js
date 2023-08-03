514140e26c527965d59fc0807c4d8c636fab8a15
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-08-03 15:20:11 +0100

    Test 3 passing: it pushes multiple 0s to the end of the array.

--- moveZeros.test.js
+++ moveZeros.test.js
@@ -8,4 +8,8 @@ describe("moveZeros", () => {
   test("it pushes a 0 to the end of the array", () => {
     expect(moveZeros([1, 0, 2])).toEqual([1, 2, 0]);
   });
+
+  test("it pushes multiple 0s to the end of the array", () => {
+    expect(moveZeros([0, 1, 0, 2, "word"])).toEqual([1, 2, "word", 0, 0]);
+  });
 });
