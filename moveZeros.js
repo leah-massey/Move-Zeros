d41b5e891e033efa42a7892d182e28121f67ed6f
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-08-03 15:19:08 +0100

    Test 2 passing: it pushes a 0 to the end of the array.

--- moveZeros.js
+++ moveZeros.js
@@ -1,5 +1,18 @@
 function moveZeros(array) {
-  return array;
+  let zeros = [];
+  let nonZeros = [];
+
+  array.forEach((value) => {
+    if (value === 0) {
+      zeros.push(value);
+    } else {
+      nonZeros.push(value);
+    }
+  });
+
+  console.log(zeros);
+  console.log(nonZeros);
+  return nonZeros.concat(zeros);
 }
 
 module.exports = moveZeros;
--- moveZeros.test.js
+++ moveZeros.test.js
@@ -4,4 +4,8 @@ describe("moveZeros", () => {
   test("it retuns an array", () => {
     expect(moveZeros([1, 2])).toEqual([1, 2]);
   });
+
+  test("it pushes a 0 to the end of the array", () => {
+    expect(moveZeros([1, 0, 2])).toEqual([1, 2, 0]);
+  });
 });
