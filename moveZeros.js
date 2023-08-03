2919abcf5ea40d46dcfb6baf8b402694a668d20c
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-08-03 15:29:42 +0100

    Refactoring of code using .filter method.

--- moveZeros.js
+++ moveZeros.js
@@ -1,17 +1,11 @@
 function moveZeros(array) {
-  let zeros = [];
-  let nonZeros = [];
-
-  array.forEach((value) => {
-    if (value === 0) {
-      zeros.push(value);
-    } else {
-      nonZeros.push(value);
-    }
+  let zeros = array.filter((value) => {
+    return value === 0;
+  });
+  let nonZeros = array.filter((value) => {
+    return value !== 0;
   });
 
-  console.log(zeros);
-  console.log(nonZeros);
   return nonZeros.concat(zeros);
 }
 
